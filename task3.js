a38b7baf3fe13f7a451ca9061e08d9091e543fb7
uhnsa <39530129+uhnsa@users.noreply.github.com>
2018-06-03 21:44:46 +0300

    Update and rename task3.js.example to task3.js

--- task3.js
+++ task3.js
@@ -1,19 +1,13 @@
 var a=Number(process.argv[2]);
 var b=Number(process.argv[3]);
-var s=0;
-var del=0;
-if(a>b){
-	s=b;
+while (a!=0 && b!=0){
+	if(a>b){
+		a=a%b;
 	}else{
-	s=a;
-	}
-while(del==0){
-	if(a%s==0&&b%s==0){
-		del=s;
-	}else{
-	s--;
+		b=b%a;
 	}
 }
+var s=a+b;
 if(s<0){
 	s=-s;
 }
